7a1518bdd58a847c9f8bbd1989cd0b7a5f28a30d
noorefrat <nour.efrat-kowalsky@uzh.ch>
2023-02-13 13:42:41 +0100

    13.2.23

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
         var fadeTime = 100;
-    
+
         /* menu controls */
-        
+
         $("#navbar-education").click(function() {
             $("#about").fadeOut(fadeTime);
             $("#project").fadeOut(fadeTime);
@@ -13,7 +13,7 @@ $(document).ready(function(){
             }, fadeTime);
             return false;
         });
-        
+
         $("#navbar-project").click(function() {
             $("#education").fadeOut(fadeTime);
             $("#about").fadeOut(fadeTime);
@@ -24,7 +24,7 @@ $(document).ready(function(){
             }, fadeTime);
             return false;
         });
-        
+
         $("#navbar-presentations").click(function() {
             $("#education").fadeOut(fadeTime);
             $("#project").fadeOut(fadeTime);
@@ -35,7 +35,7 @@ $(document).ready(function(){
             }, fadeTime);
             return false;
         });
-        
+
         $("#navbar-publications").click(function() {
             $("#education").fadeOut(fadeTime);
             $("#project").fadeOut(fadeTime);
@@ -46,14 +46,29 @@ $(document).ready(function(){
             }, fadeTime);
             return false;
         });
-        
+
         $("#transparency-in-modern-hebrew-abstract").click(function() {
             $("#transparency-in-modern-hebrew-abstract-div").slideToggle();
         });
-        
+
         $("#learnability-abstract").click(function() {
             $("#learnability-abstract-div").slideToggle();
         });
-        
-        
+
+        $("#contacttrees-abstract").click(function() {
+            $("#contacttrees-abstract-div").slideToggle();
+        });
+
+        $("#text-and-the-city-abstract").click(function() {
+            $("#text-and-the-city-abstract-div").slideToggle();
+        });
+
+        $("#transformations-abstract").click(function() {
+            $("#transformations-abstract-div").slideToggle();
+        });
+
+        $("#enclaves-abstract").click(function() {
+            $("#enclaves-abstract-div").slideToggle();
+        });
+
     });
